59e2e7c10424d961269408bef1799339e6af6541
jlgaleazzi <jgaleazzi@gmail.com>
2020-08-03 17:24:56 -0500

    fixed NaN and changed URL 2

--- energymon/src/components/consumed.jsx
+++ energymon/src/components/consumed.jsx
@@ -5,8 +5,8 @@ import propTypes from "prop-types";
 import Gauge from "./gauge";
 
 const Consumed = (props) => {
-  //const socketURL = `ws://${window.location.hostname}:5431`;
-  const socketURL = `ws://miniserver.local:5431`;
+  const socketURL = `ws://${window.location.hostname}:5431`;
+  //const socketURL = `ws://miniserver.local:5431`;
   const dispatch = useDispatch();
   const consumedSocket = new WebSocket(`${socketURL}/ccout`);
   const [time, setTime] = useState("00:00");
